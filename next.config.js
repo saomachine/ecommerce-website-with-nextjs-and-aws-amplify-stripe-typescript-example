1178752f8a76fb297275e4b8cdfe169a6047b423
saomachine <saomachine@gmail.com>
2025-06-07 12:44:34 +0200

    Update next.config.js
    Enable static export to support Amplify
    
    
    Signed-off-by: saomachine <saomachine@gmail.com>

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
   },
 };
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // This line enables static site export (no SSR)
+};
 module.exports = nextConfig;
 
 
