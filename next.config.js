baa2603e8c2ed5aab7ce5e0cc8b024c84b37b69a
saomachine <saomachine@gmail.com>
2025-06-07 13:37:58 +0200

    Fix duplicate nextConfig declaration

--- next.config.js
+++ next.config.js
@@ -8,10 +8,6 @@ const nextConfig = {
     domains: ["res.cloudinary.com", "cloudinary.com", "images.unsplash.com"],
     minimumCacheTTL: 7884000,
   },
-};
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
   output: 'export', // This line enables static site export (no SSR)
 };
 module.exports = nextConfig;
